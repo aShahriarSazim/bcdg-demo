2d05ea78161802bf1d1722958cda5aab4e4b7c03
aShahriarSazim <ahnaf.shahriar@sazim.io>
2022-06-24 21:10:30 +0600

    Added Login Route
    Added Login Route ("/login") for login user

--- client/src/routes/AppRoutes.tsx
+++ client/src/routes/AppRoutes.tsx
@@ -2,16 +2,15 @@ import {FC} from "react";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import { Home } from "../pages/Home";
 import { Register } from "../pages/Register";
+import { Login } from "../pages/Login";
 
 const AppRoutes: FC = () => {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/register" element={<Register />}></Route>
-
-            </Routes>
-        </BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Register />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+        </Routes>
     )
 }
 
