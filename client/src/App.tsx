014af7581240d4b6abbeddcbeea417965967bdc3
aShahriarSazim <ahnaf.shahriar@sazim.io>
2022-06-24 21:05:32 +0600

    Added Navigation bar and fixed App Routes
    AppRoutes component was previously wrapped inside the AppRoutes component. I did it in the App.tsx file so that I can use Navbar inside App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,16 @@ import React, {FC} from 'react';
 import {AppRoutes} from './routes';
 import {Provider} from "react-redux";
 import store from "./store/index";
+import Navbar from "./components/Navbar";
+import {BrowserRouter} from "react-router-dom";
 
 const App: FC = () => {
     return (
         <Provider store={store}>
-            <div className="app">
+            <BrowserRouter>
+                <Navbar />
                 <AppRoutes />
-            </div>
+            </BrowserRouter>
         </Provider>
     )
 }
