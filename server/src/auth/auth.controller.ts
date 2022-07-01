a84c33630f8f71642e930c51ffa6b7f772841b85
aShahriarSazim <ahnaf.shahriar@sazim.io>
2022-07-01 20:50:24 +0600

    Removed local naming and added newline at the end
    changed routes from /local/signin to /signin/, same goes for signup
    added newline at the end of the file

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,19 +1,19 @@
 import {Body, Controller, Get, Post, UseGuards, Request} from '@nestjs/common';
 import {signInDto, signUpDto} from "./dto";
 import {AuthService} from "./auth.service";
-import {AuthGuard} from "@nestjs/passport";
 
 @Controller('auth')
 export class AuthController {
 
     constructor(private AuthService: AuthService) {}
 
-    @Post('/local/signin')
+    @Post('/signin')
     async signInLocal(@Body() dto: signInDto) {
-        return this.AuthService.signInLocal(dto);
+        return this.AuthService.signIn(dto);
     }
-    @Post ('/local/signup')
+
+    @Post ('/signup')
     async signUpLocal(@Body() dto: signUpDto) {
-        return this.AuthService.signUpLocal(dto);
+        return this.AuthService.signUp(dto);
     }
 }
