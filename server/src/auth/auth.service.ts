4ea13086cf417b536823c178716c9ea19bbe1ed4
aShahriarSazim <ahnaf.shahriar@sazim.io>
2022-07-01 09:08:51 +0600

    Created signInLocal,signUpLocal,signInJwt functions
    signInLocal takes signInDto and checks if the credentials match the database. If yes, it issues a jwt token (signInJwt()) and returns that.
    signUpLocal takes signUpDto and checks if the credentials already exists in the database. If not, it will create a new user ard issue a jwt token and return it
    signInJwt takes userId and email and issues a jwt token and returns it

---
+++ server/src/auth/auth.service.ts
@@ -0,0 +1,68 @@
+import {ForbiddenException, Injectable} from '@nestjs/common';
+import {signInDto, signUpDto} from "./dto";
+import {PrismaService} from "../prisma/prisma.service";
+import {JwtService} from "@nestjs/jwt";
+import {ConfigService} from "@nestjs/config";
+
+import * as argon from 'argon2';
+import {PrismaClientKnownRequestError} from "prisma/prisma-client/runtime";
+
+@Injectable()
+export class AuthService {
+    constructor(
+        private prisma: PrismaService,
+        private jwt: JwtService,
+        private config: ConfigService
+    ) {}
+
+    async signInLocal(dto: signInDto) {
+        const user = await this.prisma.user.findUnique({
+            where: {
+                email: dto.email,
+            }
+        });
+        if(!user)throw new ForbiddenException('Invalid Credentials');
+        const isValid = await argon.verify(user.password, dto.password);
+        if(!isValid)throw new ForbiddenException('Invalid Credentials');
+
+        return await this.signInJwt(user.id, user.email);
+    }
+    async signUpLocal(dto: signUpDto) {
+        const hashedPassword = await argon.hash(dto.password);
+        try {
+            const user = await this.prisma.user.create({
+                data: {
+                    firstName: dto.firstName,
+                    lastName: dto.lastName,
+                    email: dto.email,
+                    phone: dto.phone,
+                    address: dto.address,
+                    password: hashedPassword,
+                }
+            });
+            return await this.signInJwt(user.id, user.email);
+        }catch (e) {
+            if (e instanceof PrismaClientKnownRequestError) {
+                if (e.code === 'P2002') {
+                    throw new ForbiddenException('Email or Phone already taken');
+                }
+            }
+            throw e;
+        }
+    }
+
+    async signInJwt(userId: number, email: string) {
+        const payload = {
+            userId,
+            email
+        }
+        const secret = this.config.get('JWT_SECRET');
+        const token = await this.jwt.signAsync(payload, {
+            expiresIn: '1h',
+            secret: secret
+        });
+        return {
+            access_token: token
+        };
+    }
+}
