b38abb72ba603f23bdfdf167a56f0d84dc0756dc
aShahriarSazim <ahnaf.shahriar@sazim.io>
2022-07-02 11:44:57 +0600

    updated navbar before/after authentication

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,8 +1,10 @@
 import React, {FC} from 'react';
 import {Link} from 'react-router-dom';
 import {Box, Text} from "@chakra-ui/react";
+import {useAppSelector} from "../store/hooks";
 
 const Navbar: FC = () => {
+    const auth = useAppSelector(state => state.auth);
     return (
         <Box px="20px" className="navbar" bg="green.200" h="70px" display="flex" alignItems="center" justifyContent="space-between">
             <div className="navbarHeader">
@@ -10,10 +12,19 @@ const Navbar: FC = () => {
                     <Link to="/">Teebay</Link>
                 </Text>
             </div>
-            <Box w="400px" className="navbarLinks" display="flex" justifyContent="space-between">
-                <Text><Link to="/my-products">My Products</Link></Text>
-                <Text><Link to="/login">Login</Link></Text>
-                <Text><Link to="/register">Register</Link></Text>
+            <Box  className="navbarLinks" display="flex" columnGap="20px" >
+                {auth.isAuthenticated &&
+                    <>
+                        <Text><Link to="/my-products">My Products</Link></Text>
+                        <Text><Link to="/logout">Logout</Link></Text>
+                    </>
+                }
+                {!auth.isAuthenticated &&
+                    <>
+                        <Text><Link to="/login">Login</Link></Text>
+                        <Text><Link to="/register">Register</Link></Text>
+                    </>
+                }
             </Box>
         </Box>
     )
