2c6d890e1592e0ad7cb5554b6fb132666e97172c
aShahriarSazim <ahnaf.shahriar@sazim.io>
2022-07-04 18:19:11 +0600

    simplified the error status and added getCurrentLoggedInUser()

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -5,7 +5,6 @@ import {JwtService} from "@nestjs/jwt";
 import {ConfigService} from "@nestjs/config";
 
 import * as argon from 'argon2';
-import {PrismaClientKnownRequestError} from "prisma/prisma-client/runtime";
 
 @Injectable()
 export class AuthService {
@@ -42,12 +41,11 @@ export class AuthService {
             });
             return await this.generateJWT(user.id, user.email);
         }catch (e) {
-            if (e instanceof PrismaClientKnownRequestError) {
-                if (e.code === 'P2002') {
-                    throw new ForbiddenException('Email or Phone already taken');
-                }
+
+            if (e.code === 'P2002') {
+                throw new ForbiddenException(e.meta.target);
             }
-            throw e;
+            throw new ForbiddenException(e);
         }
     }
 
@@ -64,5 +62,24 @@ export class AuthService {
         return {
             access_token: token
         };
+    }
+
+    async getCurrentLoggedInUser(userId) {
+        try {
+            return await this.prisma.user.findUnique({
+                where: {
+                    id: userId
+                },
+                select: {
+                    firstName: true,
+                    lastName: true,
+                    email: true,
+                    phone: true,
+                    address: true
+                }
+            });
+        }catch(e){
+            throw new ForbiddenException('Invalid Credentials');
+        }
     }
 }
