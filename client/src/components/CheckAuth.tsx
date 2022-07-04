120df2e1011418f2cbb069428b5bdecc8ba757db
aShahriarSazim <ahnaf.shahriar@sazim.io>
2022-07-04 18:46:30 +0600

    used ternary operator to reduce the code size and increase code readability

--- client/src/components/CheckAuth.tsx
+++ client/src/components/CheckAuth.tsx
@@ -5,23 +5,23 @@ import {clearAuth, setAuth} from "../store/slices/AuthSlice";
 
 const CheckAuth: FC = () => {
     const dispatch = useAppDispatch();
-    useEffect(() => {
 
-        const checkUserAuthentication = async () => {
-            try {
-                const response = await axios.get("/auth/current-loggedin-user");
-                if(response.data){
-                    dispatch(setAuth(response.data));
-                }else{
-                    localStorage.removeItem("access_token");
-                    dispatch(clearAuth());
-                }
-            }catch(e){
+    const checkUserAuthentication = async () => {
+        try {
+            const response = await axios.get("/auth/current-loggedin-user");
+            if(response.data){
+                dispatch(setAuth(response.data));
+            }else{
                 localStorage.removeItem("access_token");
                 dispatch(clearAuth());
             }
-        };
+        }catch(e){
+            localStorage.removeItem("access_token");
+            dispatch(clearAuth());
+        }
+    };
 
+    useEffect(() => {
         const token = localStorage.getItem("access_token");
 
         // If token exists, check if the token is valid.
