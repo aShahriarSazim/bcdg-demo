d20f2af9fa33dcbcccaecca5b9fe6d6f88bc86a4
aShahriarSazim <ahnaf.shahriar@sazim.io>
2022-07-04 22:30:18 +0600

    removed unused interface files and refactored UserInterface, RegisterDataInterface, LoginDataInterface. It's more reacable now

---
+++ client/src/Interfaces/RegisterDataInterface/RegisterDataInterface.ts
@@ -0,0 +1,9 @@
+export interface RegisterDataInterface {
+    firstName?: string;
+    lastName?: string;
+    email?: string;
+    address?: string;
+    phone?: string;
+    password?: string;
+    confirmPassword?: string;
+}
