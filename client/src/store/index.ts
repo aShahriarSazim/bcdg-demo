61bf9a6992b753de7d46865975f553c9446ae888
aShahriarSazim <ahnaf.shahriar@sazim.io>
2022-07-04 18:52:24 +0600

    removed RegisterDataReducer since I am not using redux for register state management

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import AuthReducer from './slices/AuthSlice';
-import RegisterDataReducer from './slices/RegisterDataSlice';
 
 export const store = configureStore({
     reducer: {
-        auth: AuthReducer,
-        registerData: RegisterDataReducer
+        auth: AuthReducer
     },
 });
 
