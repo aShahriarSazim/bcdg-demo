1f777ea5e9ce14449dce939b388bf4dd32576719
aShahriarSazim <ahnaf.shahriar@sazim.io>
2022-07-08 00:52:23 +0600

    changed a minor text

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -6,7 +6,7 @@ const Home: FC = () => {
     return (
         <Box my={20} mx={20}>
             <Text textAlign={'center'} fontWeight={'bold'} fontSize={30}>Home Page</Text>
-            <Text textAlign={'center'} mt={10}>Here are some Descriptions. You can add some introductory things here</Text>
+            <Text textAlign={'center'} mt={10}>Here are some Descriptions. You can add some introductory things here. Or some images too</Text>
             <Text mt={10} textAlign={'center'}><Link to={'/products'}><Button colorScheme={'green'}>See All Products</Button></Link></Text>
         </Box>
     )
