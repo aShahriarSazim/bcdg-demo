1d766bc8741a34f2c1ad009f10b368963cca51d2
aShahriarSazim <ahnaf.shahriar@sazim.io>
2022-07-08 00:39:09 +0600

    added FetchAllProducts component that fetches all products while rendering the app, and no additional calls will be made

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,12 +5,14 @@ import store from "./store/index";
 import Navbar from "./components/Navbar";
 import {BrowserRouter} from "react-router-dom";
 import CheckAuth from "./components/CheckAuth";
+import FetchAllProducts from "./components/FetchAllProducts";
 
 const App: FC = () => {
     return (
         <Provider store={store}>
             <BrowserRouter>
                 <CheckAuth />
+                <FetchAllProducts />
                 <Navbar />
                 <AppRoutes />
             </BrowserRouter>
