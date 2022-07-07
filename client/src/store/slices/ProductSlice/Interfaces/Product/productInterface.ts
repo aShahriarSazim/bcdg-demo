21624de06c02e21152d1364c28206412084e7aba
aShahriarSazim <ahnaf.shahriar@sazim.io>
2022-07-08 00:46:51 +0600

    created ProductSlice where I'm storing all the product data.

---
+++ client/src/store/slices/ProductSlice/Interfaces/Product/productInterface.ts
@@ -0,0 +1,39 @@
+interface categoryInterface{
+    category: {
+        id: number;
+        name: string;
+    }
+}
+interface rentHistoryInterface{
+    id: number;
+    user: {
+        id: number;
+        name: string;
+    }
+}
+
+export default interface ProductInterface{
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+    rent: number;
+    rentPaymentPeriod: number;
+    views: 0;
+    status: boolean;
+    createdAt: string;
+    updatedAt: string;
+    user: {
+        id: number;
+        email: string;
+    }
+    categories: categoryInterface[],
+    purchaseHistory ?: {
+        id: number;
+        user: {
+            id: number;
+            email: string;
+        }
+    },
+    rentHistory ?: rentHistoryInterface[]
+}
