4b912d3a33ef7a16d0794f04c703950927e7cbf4
aShahriarSazim <ahnaf.shahriar@sazim.io>
2022-07-13 23:21:16 +0600

    removed previous manual method and added createAsyncThunk functionality for all product-related pages and components

--- client/src/pages/Products/MyProducts/MyProducts.tsx
+++ client/src/pages/Products/MyProducts/MyProducts.tsx
@@ -1,37 +1,47 @@
 import {FC, useEffect, useState} from "react";
 import {Box, Button, Text} from "@chakra-ui/react";
 import ProductInterface from "../../../store/slices/ProductSlice/Interfaces/Product/productInterface";
-import axios from "../../../axios";
-import Product from "../components/Product";
-import {useAppSelector} from "../../../store/hooks";
-import {Link, useNavigate} from "react-router-dom";
+import ProductCard from "../components/ProductCard";
+import {useAppDispatch, useAppSelector} from "../../../store/hooks";
+import {Link} from "react-router-dom";
+import {getAllProducts} from "../../../store/slices/ProductSlice/AllProducts";
 
 const MyProducts: FC = () => {
+    const dispatch = useAppDispatch();
     const auth = useAppSelector(state => state.auth);
-    const myProducts = useAppSelector(state => state.products).filter(
-        product => product.user.email === auth.email
-    );
-    return (
-        <Box py="20px" px="20px">
-            <Text textAlign="center" fontSize="44px" fontWeight="bold">My Products</Text>
-            <Box px={`20px`} mt="20px" width={`750px`} mx={'auto'}>
-                <Link to={`/products/create`}>
-                    <Button colorScheme={`green`}> Create New Product</Button>
-                </Link>
+    // I haven't made any api endpoints for userProducts in the backend yet. So, I'm currently taking all products and filtering them by user email. However, after finishing this PR, I will make an api endpoint for userProducts and update this.
+    const allProducts = useAppSelector(state => state.products);
+
+    useEffect(() => {
+        dispatch(getAllProducts());
+    }, []);
+    if(allProducts.loading){
+        return (
+            <Text>Loading....</Text>
+        )
+    }else {
+        return (
+            <Box py="20px" px="20px">
+                <Text textAlign="center" fontSize="44px" fontWeight="bold">My Products</Text>
+                <Box px={`20px`} mt="20px" width={`750px`} mx={'auto'}>
+                    <Link to={`/products/create`}>
+                        <Button colorScheme={`green`}> Create New Product</Button>
+                    </Link>
+                </Box>
+                <Box my="20px">
+                    {allProducts.data.map((product: ProductInterface, index) => {
+                        if (product.user.email === auth.email) {
+                            return (
+                                <Box key={index} p="20px" mx="auto" width="750px">
+                                    <ProductCard {...product} />
+                                </Box>
+                            )
+                        }
+                    })}
+                </Box>
             </Box>
-            <Box my="20px" >
-                {myProducts.map((product: ProductInterface, index) => {
-                    if(product.user.email === auth.email){
-                        return(
-                            <Box key={index} p="20px" mx="auto" width="750px">
-                                <Product {...product} />
-                            </Box>
-                        )
-                    }
-                })}
-            </Box>
-        </Box>
-    )
+        )
+    }
 }
 
 export default MyProducts;
--- client/src/pages/Products/Products.tsx
+++ client/src/pages/Products/Products.tsx
@@ -1,30 +1,38 @@
-import {FC, useEffect, useState} from "react";
+import {FC, useEffect} from "react";
 import ProductInterface from "../../store/slices/ProductSlice/Interfaces/Product/productInterface";
-import axios from "../../axios";
 import {Box, Text} from "@chakra-ui/react";
-import Product from "./components/Product";
-import {useAppSelector} from "../../store/hooks";
+import ProductCard from "./components/ProductCard";
+import {useAppDispatch, useAppSelector} from "../../store/hooks";
+import {getAllProducts} from "../../store/slices/ProductSlice/AllProducts";
 
 const Products: FC = () => {
 
+    const dispatch = useAppDispatch();
     const allProducts = useAppSelector(state => state.products);
 
     useEffect(() => {
+        dispatch(getAllProducts());
     }, []);
 
-    return (
-        <Box py="20px" px="20px">
-            <Text textAlign="center" fontSize="44px" fontWeight="bold">All Products</Text>
-            <Box my="20px" >
-                {allProducts.map((product: ProductInterface, index) => {
-                   return(
-                   <Box key={index} p="20px" mx="auto" width="750px">
-                       <Product {...product} />
-                   </Box>)
-                })}
+    if(allProducts.loading){
+        return (
+            <Text>Loading....</Text>
+        )
+    }else{
+        return (
+            <Box py="20px" px="20px">
+                <Text textAlign="center" fontSize="44px" fontWeight="bold">All Products</Text>
+                <Box my="20px" >
+                    {allProducts.data.map((product: ProductInterface, index) => {
+                        return(
+                            <Box key={index} p="20px" mx="auto" width="750px">
+                                <ProductCard {...product} />
+                            </Box>)
+                    })}
+                </Box>
             </Box>
-        </Box>
-    )
+        )
+    }
 }
 
 export default Products;
