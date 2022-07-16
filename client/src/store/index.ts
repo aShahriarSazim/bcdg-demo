2a4b35464e12be55f9004e8a5ae9506aaa6dd046
aShahriarSazim <ahnaf.shahriar@sazim.io>
2022-07-16 13:11:22 +0600

    added userProductSlice in store

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import AuthReducer from './slices/AuthSlice/AuthSlice';
 import AllProducts from './slices/ProductSlice/AllProducts/AllProducts';
 import ProductById from "./slices/ProductSlice/ProductById/ProductById";
+import userProductSlice from "./slices/ProductSlice/UserProducts/UserProducts";
 
 export const store = configureStore({
     reducer: {
         auth: AuthReducer,
         products: AllProducts,
+        userProducts: userProductSlice,
         product: ProductById,
     },
 });
