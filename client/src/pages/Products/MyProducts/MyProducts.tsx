d6c0a6663e8fb60a33d988c1fdea53868786c030
aShahriarSazim <ahnaf.shahriar@sazim.io>
2022-07-16 13:12:19 +0600

    added tab system inside MyProucts page

--- client/src/pages/Products/MyProducts/MyProducts.tsx
+++ client/src/pages/Products/MyProducts/MyProducts.tsx
@@ -1,47 +1,43 @@
-import {FC, useEffect} from "react";
-import {Box, Button, Text} from "@chakra-ui/react";
-import ProductInterface from "../../../store/slices/ProductSlice/Interfaces/Product/productInterface";
-import ProductCard from "../components/ProductCard";
-import {useAppDispatch, useAppSelector} from "../../../store/hooks";
-import {Link} from "react-router-dom";
-import {getAllProducts} from "../../../store/slices/ProductSlice/AllProducts";
+import {FC} from "react";
+import {Box, Tabs, TabList, TabPanels, Tab, TabPanel} from "@chakra-ui/react";
+import UserProducts from "./components/UserProducts";
+import UserBoughtProducts from "./components/UserBoughtProducts";
+import UserSoldProducts from "./components/UserSoldProducts";
+import UserRentProducts from "./components/UserRentProducts";
+import UserLentProducts from "./components/UserLentProducts";
 
 const MyProducts: FC = () => {
-    const dispatch = useAppDispatch();
-    const auth = useAppSelector(state => state.auth);
-    // I haven't made any api endpoints for userProducts in the backend yet. So, I'm currently taking all products and filtering them by user email. However, after finishing this PR, I will make an api endpoint for userProducts and update this.
-    const allProducts = useAppSelector(state => state.products);
+    return (
+        <Box mt={5} >
+            <Tabs variant={'enclosed'} isLazy={true}>
+                <TabList>
+                    <Tab>My Products</Tab>
+                    <Tab>Bought Products</Tab>
+                    <Tab>Sold Products</Tab>
+                    <Tab>Rented Products</Tab>
+                    <Tab>Lent Products</Tab>
+                </TabList>
 
-    useEffect(() => {
-        dispatch(getAllProducts());
-    }, []);
-    if(allProducts.loading){
-        return (
-            <Text>Loading....</Text>
-        )
-    }else {
-        return (
-            <Box py="20px" px="20px">
-                <Text textAlign="center" fontSize="44px" fontWeight="bold">My Products</Text>
-                <Box px={`20px`} mt="20px" width={`750px`} mx={'auto'}>
-                    <Link to={`/products/create`}>
-                        <Button colorScheme={`green`}> Create New Product</Button>
-                    </Link>
-                </Box>
-                <Box my="20px">
-                    {allProducts.data.map((product: ProductInterface, index) => {
-                        if (product.user.email === auth.email) {
-                            return (
-                                <Box key={index} p="20px" mx="auto" width="750px">
-                                    <ProductCard {...product} />
-                                </Box>
-                            )
-                        }
-                    })}
-                </Box>
-            </Box>
-        )
-    }
+                <TabPanels>
+                    <TabPanel>
+                        <UserProducts></UserProducts>
+                    </TabPanel>
+                    <TabPanel>
+                        <UserBoughtProducts></UserBoughtProducts>
+                    </TabPanel>
+                    <TabPanel>
+                        <UserSoldProducts></UserSoldProducts>
+                    </TabPanel>
+                    <TabPanel>
+                        <UserRentProducts></UserRentProducts>
+                    </TabPanel>
+                    <TabPanel>
+                        <UserLentProducts></UserLentProducts>
+                    </TabPanel>
+                </TabPanels>
+            </Tabs>
+        </Box>
+    )
 }
 
 export default MyProducts;
