cf2b0f330a90a196ca7e302245780f619f7afeb7
aShahriarSazim <ahnaf.shahriar@sazim.io>
2022-07-25 10:21:08 +0600

    changed app name and theme

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -6,10 +6,10 @@ import {useAppSelector} from "../store/hooks";
 const Navbar: FC = () => {
     const auth = useAppSelector(state => state.auth);
     return (
-        <Box px="20px" className="navbar" bg="green.200" h="70px" display="flex" alignItems="center" justifyContent="space-between">
+        <Box px="20px" className="navbar" bg="blackAlpha.700" h="70px" display="flex" alignItems="center" justifyContent="space-between" color={"white"}>
             <div className="navbarHeader">
                 <Text fontSize="25px" fontWeight="bold">
-                    <Link to="/">Teebay</Link>
+                    <Link to="/">Games Hive</Link>
                 </Text>
             </div>
             <Box  className="navbarLinks" display="flex" columnGap="20px" >
