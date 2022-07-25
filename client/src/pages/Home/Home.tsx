537e7a58b561d58fd2d89c76c21a84509ea560de
aShahriarSazim <107447519+aShahriarSazim@users.noreply.github.com>
2022-07-25 19:09:08 +0600

    changed app name and theme (#12)

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -5,7 +5,7 @@ import {Link} from "react-router-dom";
 const Home: FC = () => {
     return (
         <Box my={20} mx={20}>
-            <Text textAlign={'center'} fontWeight={'bold'} fontSize={30}>Home Page</Text>
+            <Text textAlign={'center'} fontWeight={'bold'} fontSize={30}>The ultimate place to buy gaming products.</Text>
 
             <Text textAlign={'center'} mt={10}>Here are some Descriptions. You can add some introductory things here. Or some images too</Text>
             <Text mt={10} textAlign={'center'}><Link to={'/products'}><Button colorScheme={'green'}>See All Products</Button></Link></Text>
