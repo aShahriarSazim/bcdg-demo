2c0c70f4de768dac2e48c27aad8c3fc584cbe740
aShahriarSazim <ahnaf.shahriar@sazim.io>
2022-07-26 18:10:48 +0600

    change jwt expireIn from 1h to 5h

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -56,7 +56,7 @@ export class AuthService {
         }
         const secret = this.config.get('JWT_SECRET');
         const token = await this.jwt.signAsync(payload, {
-            expiresIn: '1h',
+            expiresIn: '5h',
             secret: secret
         });
         return {
